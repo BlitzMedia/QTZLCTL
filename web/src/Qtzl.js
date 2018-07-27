09a8670575155ac50901c9d3a76d5e597e5bc156
gorkamolero <gorka.molero@gmail.com>
2018-07-27 18:56:28 +0200

    go!

--- web/src/Qtzl.js
+++ web/src/Qtzl.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import './styles/globalStyles'
 import slugify from 'slugify'
 
@@ -83,24 +83,31 @@ class Qtzl extends Component {
       <div>
         <QTZLCTL hide={loaded} />
 
-        <Route exact path='/' render={({ match }) => (
-          <Home content={releaseContent} links={links} match={match} />
-        )} />
+        <Switch>
 
-        <Route exact path='/artists' render={({ match }) => (
-          <Home content={artistContent} links={links} match={match} />
-        )} />
+          <Route exact path='/' render={({ match }) => (
+            <Home content={releaseContent} links={links} match={match} />
+          )} />
 
-        <Route exact path='/releases/:id' render={({ match }) => {
-          const release = releases.find(r => r.fields.Release === match.params.id)
-          return <Release release={release} id={match.params.id} />
-        }} />
+          <Route exact path='/artists' render={({ match }) => (
+            <Home content={artistContent} links={links} match={match} />
+          )} />
 
-        <Route exact path='/artists/:slug' render={({ match }) => {
-          const artist = artists.find(a => a.slug === match.params.slug)
-          const releases = this.state.releases.filter(r => r.fields['Artist Name'][0] === artist.fields['Name'])
-          return <Artist artist={artist} releases={releases}/>
-        }} />
+          <Route exact path='/releases/:id' render={({ match }) => {
+            const release = releases.find(r => r.fields.Release === match.params.id)
+            return <Release release={release} id={match.params.id} />
+          }} />
+
+          <Route exact path='/artists/:slug' render={({ match }) => {
+            const artist = artists.find(a => a.slug === match.params.slug)
+            const releases = this.state.releases.filter(r => r.fields['Artist Name'][0] === artist.fields['Name'])
+            return <Artist artist={artist} releases={releases}/>
+          }} />
+
+          <Route render={({ match }) => (
+            <Home content={releaseContent} links={links} match={match} />
+          )} />
+        </Switch>
       </div>
     )
   }
